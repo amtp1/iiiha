74c7842f85056c6b0dd903b9b09892d975406b2d
amtp1 <magomedov.abdul20012@gmail.com>
2024-01-21 16:49:52 +0300

    add SmartCamera model

--- iiiha/core/models.py
+++ iiiha/core/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+from os.path import splitext
+from uuid import uuid4
 
-# Create your models here.
+from django.db import models
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+
+
+class UUIDFileStorage(FileSystemStorage):
+    def get_available_name(self, name, max_length=None) -> str:
+        _, ext = splitext(name)
+        return F"{settings.MEDIA_ROOT}/{uuid4().hex + ext}"
+
+
+class SmartCamera(models.Model):
+    image = models.ImageField(storage=UUIDFileStorage())
